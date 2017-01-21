78f35f7bce7e6df7ed9b094e9a9e87f119c8b5cd
Peter Rexer <prexer@gmail.com>
2017-01-20 21:07:05 -0800

    updated with path and startup stuff to make it actually work on my pi.

--- watch.sh
+++ watch.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+ps -ef | grep key_event_writer | grep -v grep | awk '{print $2}' |xargs kill
+cd /home/pi/Vision
 /home/pi/.virtualenvs/cv/bin/python key_event_writer.py --conf conf.json
