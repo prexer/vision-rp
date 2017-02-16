9f42ebca859bfe6dc24f4a533be6039b1a92e1a8
prexer <prexer@gmail.com>
2017-02-15 21:15:53 -0800

    fixed upload datestamp for mkv files

--- key_event_writer.py
+++ key_event_writer.py
@@ -173,7 +173,7 @@ while True:
         #if Dropbox is turned on, upload the file
         if conf["use_dropbox"]:
             path = "{base_path}/{ds}/ {timestamp}.{extension}".format(
-                            base_path=conf["dropbox_base_path"], timestamp=ts,
+                            base_path=conf["dropbox_base_path"],ds=filedate, timestamp=ts,
                             extension=conf["filetype"])
             uploader.queue_file( p, path, ts)
 
